7dbf54fd9fa01dacd8565d57e90e75e750cdbdb7
MISVIS <sramisvis@gmail.com>
2023-10-09 09:22:34 +0800

    move message

--- resources/views/dashboard/budget/moved.blade.php
+++ resources/views/dashboard/budget/moved.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 
     <section class="content">
-
         <div class='text-center' style="margin-top:10%;">
             <h1><span style="font-size: 120px; color: grey"><i class="fa fa-exclamation-triangle"></i></span></h1>
             <h1>Migration Notice</h1>
@@ -11,8 +10,8 @@
             <h3><a href="http://budget.sra.gov.ph/"> http://budget.sra.gov.ph/</a> </h3>
             <br>
             <p><a href="http://10.36.1.14:8010/"> http://10.36.1.14:8010/</a> for local use in SRA Bacolod Network</p>
+            <h5 class="text-danger"><i class="fa fa-warning"></i> <b>This module will soon be removed from SWEP-AFD</b></h5>
         </div>
-
     </section>
 
 @endsection
