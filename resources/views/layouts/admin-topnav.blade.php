f4b22bdfd3fad73b8a0852ab63b42ec4188490e9
MISVIS <sramisvis@gmail.com>
2023-10-10 09:55:07 +0800

    lobby

--- resources/views/layouts/admin-topnav.blade.php
+++ resources/views/layouts/admin-topnav.blade.php
@@ -31,7 +31,7 @@
           <li style="width: 750px;padding-top: 12px"><p style="color: white; font-size: larger">DEVELOPMENT MODE</p></li>
         @endif
           <li class="dropdown tasks-menu">
-            <a href="http://{{$_SERVER['SERVER_NAME']}}/" >
+            <a href="http://swep.sra.gov.ph/" >
               <i class="fa fa-home"></i> Lobby Page
             </a>
           </li>
