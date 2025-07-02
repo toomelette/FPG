61e0a2d51905eef24df1c430d5abe9416b84fdd5
Gerald <gguance221@gmail.com>
2025-07-02 15:28:49 +0800

    wss

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
 //                'useTLS' => true,
-                'host' => 'hrrs.sra.gov.ph',
+                'host' => '127.0.0.1',
                 'port' => env('SWEP_AFD_WS_PORT'),
                 'scheme' => 'http',
                 'encrypted' => true,
