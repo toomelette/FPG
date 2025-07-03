65ea76e1a070d9d6184c0335ab9fb94904df2e3f
Gerald <gguance221@gmail.com>
2025-07-03 09:51:03 +0800

    testmail

--- config/mail.php
+++ config/mail.php
@@ -57,11 +57,11 @@ return [
 
         'testgov' => [
             'transport' => 'smtp',
-            'host' => 'mail.sra.gov.ph',
+            'host' => 'smtp.google.com',
             'port' => 465,
             'encryption' => 'tls',
-            'username' => 'info@sra.gov.ph',
-            'password' => 'Thispasswordisintendedforinfo',
+            'username' => 'mis-vis@sra.gov.ph',
+            'password' => 'ppspd_misvis',
             'timeout' => null,
             'auth_mode' => null,
         ],
