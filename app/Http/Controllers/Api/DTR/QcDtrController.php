03a96a530a812f79db3e5fc28dd52f2ab8e39697
Gerald <gguance221@gmail.com>
2025-07-10 14:09:24 +0800

    recev

--- app/Http/Controllers/Api/DTR/QcDtrController.php
+++ app/Http/Controllers/Api/DTR/QcDtrController.php
@@ -3,13 +3,39 @@
 namespace App\Http\Controllers\Api\DTR;
 
 use App\Http\Controllers\Controller;
+use App\Models\DailyTimeRecord;
+use App\Models\DTR;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class QcDtrController extends Controller
 {
     public function store(Request $request)
     {
-        return $request->all();
-        dd($request->all(),1);
+        $dailyTimeRecords = $request['daily_time_records'];
+        $dtrs = $request['dtrs'];
+        $batchId = 'QC'.Str::random(8);
+
+        $dailyTimeRecordsArray = [];
+        if(!empty($dailyTimeRecords) ){
+            foreach ($dailyTimeRecords as $dailyTimeRecord){
+                $dailyTimeRecord['system_remarks'] = $batchId;
+                $dailyTimeRecordsArray[] = $dailyTimeRecord;
+            }
+        }
+
+        $dtrsArray = [];
+        if(!empty($dtrs) ){
+            foreach ($dtrs as $dtr){
+                $dtr['system_remarks'] = $batchId;
+                $dtrsArray[] = $dtr;
+            }
+        }
+
+        DailyTimeRecord::query()->insert($dailyTimeRecordsArray);
+        DTR::query()->insert($dtrsArray);
+        return [
+            'message' => 'DTRs successfully uploaded.',
+        ];
     }
 }
