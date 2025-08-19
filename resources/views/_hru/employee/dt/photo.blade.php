5dce8e0a92b2fa2b0d3635d8adc6dfffb0bcaf71
Gerald <gguance221@gmail.com>
2025-08-19 10:20:15 +0800

    avatar

--- resources/views/_hru/employee/dt/photo.blade.php
+++ resources/views/_hru/employee/dt/photo.blade.php
@@ -2,7 +2,15 @@
 @if($data->photo != null)
         @if(File::exists(public_path('symlink/employee_pics/uploaded_300/'.$data->photo)))
                 <center>
-                        <img class="gj-imagebox" src='/symlink/employee_pics/uploaded_300/{{$data->photo}}' original="/symlink/employee_pics/uploaded/{{$data->photo}}"  style="object-fit: cover;" width="100" height="100" alt="User Image">
+                        <img class="gj-imagebox rounded" src='/symlink/employee_pics/uploaded_300/{{$data->photo}}' original="/symlink/employee_pics/uploaded/{{$data->photo}}"  style="object-fit: cover;" width="100" height="100" alt="User Image">
+                </center>
+        @else
+                <center>
+                <img class="gj-imagebox rounded" src='/images/avatar{{$data->sex}}.png'   style="object-fit: cover;" width="100" height="100" alt="User Image">
                 </center>
         @endif
+@else
+        <center>
+                <img class="gj-imagebox rounded" src='/images/avatar{{$data->sex}}.png'   style="object-fit: cover;" width="100" height="100" alt="User Image">
+        </center>
 @endif
