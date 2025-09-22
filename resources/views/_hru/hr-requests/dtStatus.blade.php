9d74d13ab338cebb175464cc2cf108a587d9ff46
Gerald <gguance221@gmail.com>
2025-09-22 11:49:40 +0800

    file upload in HR Request

--- resources/views/_hru/hr-requests/dtStatus.blade.php
+++ resources/views/_hru/hr-requests/dtStatus.blade.php
@@ -5,6 +5,9 @@
     @case('FOR SIGNATURE')
         <span class="badge bg-info" style="width: 100%;">{{$data->status}}</span>
         @break
+    @case('REQUEST SUBMITTED')
+        <span class="badge bg-primary" style="width: 100%;">{{$data->status}}</span>
+        @break
     @case('READY FOR PICK-UP')
         <span class="badge bg-success" style="width: 100%;">{{$data->status}}</span>
         @break
