9398bb6fc28326dd52f602e54b68d3553eb3b427
Gerald <gguance221@gmail.com>
2025-09-24 14:08:56 +0800

    other actions HR COE COmpen

--- resources/views/printables/employee/coe_with_compensation.blade.php
+++ resources/views/printables/employee/coe_with_compensation.blade.php
@@ -1,150 +1,170 @@
-<html>
-<head>
-    <style>
-        body{
-            font-family: Cambria !important;
-        }
+@extends('printables.print_layouts.print_layout_main')
 
-        .edit_form{
-            margin-bottom: 0px;
-        }
-
-    </style>
-    <link type="text/css" rel="stylesheet" href="{{asset('css/print.css')}}?rand={{\Illuminate\Support\Str::random()}}">
-    <script type="text/javascript" src="{{ asset('template/bower_components/jquery/dist/jquery.min.js') }}"></script>
-    <title>
-        CERTIFICATE OF EMPLOYMENT
-    </title>
-</head>
-<body style="padding-top: 85px; ">
-
-<p class="text-right">{{\Illuminate\Support\Carbon::now()->format('F d, Y')}}</p>
-
-<p class="text-strong text-center">CERTIFICATE OF EMPLOYMENT AND COMPENSATION</p>
-<br><br>
-<p style="text-indent: 40px; text-align: justify; line-height: 30px">This is to certify that
-    <u>
-        <b>
-            {{$employee->firstname}}
-            @if(strlen( $employee->middlename < 2))
-                {{$employee->middlename}}.
-            @else
-                {{\Illuminate\Support\Str::limit($employee->middlename,1,'.')}}
-            @endif
-            {{$employee->lastname}}
-        </b>
-    </u>
-
-    is an employee of SUGAR REGULATORY ADMINISTRATION since
-    <u>
-        <b>
-            {{\Illuminate\Support\Carbon::parse($employee->firstday_sra)->format('F d, Y')}}
-        </b>
-    </u>
-    and up to the present.
-
-    {{($employee->sex == 'FEMALE') ? 'She' : 'He'}}
-
-    holds a Permanent Appointment of
-    <u>
-        <b>
-            {{strtoupper($employee->position)}}.
-        </b>
-    </u>
-</p>
-
-<p style="text-indent: 40px; text-align: justify; line-height: 30px">
-    {{($employee->sex == 'FEMALE' ? 'Her' : 'His')}}
-    present monthly compensation are as follows:
-</p>
-
-@php
-    $monthlyTemplate = $employee->incentiveTemplate()->whereHas('incentive',function($q){
-            return $q->where('IsMonthly','=',1);
-        })
-        ->where('IncAmount','!=',0)
-        ->orderBy('IncCode','asc')
-        ->get();
-    $yearlyTemplate = $employee->incentiveTemplate()->whereHas('incentive',function($q){
-            return $q->where('IsMonthly','=',0);
-        })
-        ->where('IncAmount','!=',0)
-        ->orderBy('IncCode','asc')
-        ->get();
-
-@endphp
-
-<table style="width: 100%; padding: 0 70px">
-    <tbody>
-    @php
-        $total = 0;
-    @endphp
-    @if(!empty($monthlyTemplate))
-        @foreach($monthlyTemplate as $incentive)
-            @php
-                $total = $total + $incentive->IncAmount
-            @endphp
-            <tr>
-                <td>{{$incentive->incentive->IncDesc ?? 'N/A'}}</td>
-                <td class="text-right">{{number_format($incentive->IncAmount,2)}}</td>
-            </tr>
-        @endforeach
-        <tr>
-            <td class="text-strong text-center b-top" >TOTAL</td>
-            <td class="text-strong text-right b-top" >{{number_format($total,2)}}</td>
-        </tr>
-    @endif
-    </tbody>
-</table>
-
-<p style="text-indent: 40px; text-align: justify; line-height: 30px">
-    Further,
-    {{($employee->sex == 'FEMALE' ? 'she' : 'he')}}
-    receives the following additional yearly remuneration:
-</p>
-
-<table style="width: 100%; padding: 0 70px">
-    <tbody>
-    @php
-        $total = 0;
-    @endphp
-    @if(!empty($yearlyTemplate))
-        @foreach($yearlyTemplate as $incentive)
-            @php
-                $total = $total + $incentive->IncAmount
-            @endphp
-            <tr>
-                <td>{{$incentive->incentive->IncDesc ?? 'N/A'}}</td>
-                <td class="text-right">{{number_format($incentive->IncAmount,2)}}</td>
-            </tr>
-        @endforeach
-        <tr>
-            <td class="text-strong text-center b-top" >TOTAL</td>
-            <td class="text-strong text-right b-top" >{{number_format($total,2)}}</td>
-        </tr>
-    @endif
-    </tbody>
-</table>
-
-
-<p style="text-indent: 40px">
-    This certification is issued for whatever legal purpose it may serve.
-</p>
-<br><br>
-<div style="overflow: auto">
-    <div style="width: 40%; float: right">
-        <p class="text-center">
-            <b>{{\Illuminate\Support\Facades\Request::get('signatory_name')}}</b>
-            <br>
-            {{\Illuminate\Support\Facades\Request::get('signatory_position')}}
+@section('wrapper')
+    <div style="font-family: Cambria; font-size: 16px; margin: 170px 40px 0px 40px">
+        <p style="text-align: right; line-height: 38px">
+            {{Carbon::now()->format('F d, Y')}}
         </p>
+        <br>
+        <p class="text-strong text-center" style="letter-spacing: 1px; font-size: 22px">CERTIFICATE OF EMPLOYMENT AND COMPENSATION</p>
+        <br>
+
+        <p style="text-indent: 40px; line-height: 20px; text-align: justify">
+        This is to certify that  {{$employee->sex == 'MALE' ? 'MR.' : 'MS.'}} {{$employee->full['FMiLE']}} has been an employee of the Sugar Regulatory Administration since
+        {{Carbon::parse($employee->firstday_sra)->format('F d, Y')}}, to date.
+        </p>
+
+        {{$employee->sex == 'MALE' ? 'He' : 'She'}} holds a permanent appointment as {{$employee->plantilla->position}}.
+        <br><br>
+        <p style="text-indent: 40px; line-height: 20px; text-align: justify">
+            {{$employee->sex == 'MALE' ? 'His' : 'Her'}} present monthly compensation are as follows:
+        </p>
+        @php
+            $employee->load('templateIncentives.incentive','payrollSettings');
+            $monthly = $employee->templateIncentives->where('incentive_code','MONTHLY')?->first()?->amount ?? 0;
+            $pera = $employee->templateIncentives->where('incentive_code','PERA')?->first()?->amount ?? 0;
+            $ra = $employee?->payrollSettings?->ra_rate ?? 0;
+            $ta = $employee?->payrollSettings?->ta_rate ?? 0;
+        @endphp
+        <table style="width: 80%; margin-left: 15%; font-size: 15px">
+            <tr>
+                <td style="width: 60%">Basic Salary</td>
+                <td class="text-right">{{\App\Swep\Helpers\Helper::toNumber($monthly)}}</td>
+                <td style="width: 60px">/month</td>
+                <td style="width: 100px"></td>
+                <td style="width: 60px"></td>
+            </tr>
+            <tr>
+                <td>PERA</td>
+                <td class="text-right">{{\App\Swep\Helpers\Helper::toNumber($pera)}}</td>
+                <td>/month</td>
+                <td></td>
+                <td></td>
+            </tr>
+
+            @if(!empty($employee->payrollSettings->receives_ra))
+                <tr>
+                    <td>Representation</td>
+                    <td class="text-right">{{\App\Swep\Helpers\Helper::toNumber($ra)}}</td>
+                    <td>/month</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            @endif
+            @if(!empty($employee->payrollSettings->receives_ta))
+                <tr>
+                    <td>Transportation</td>
+                    <td class="text-right">{{\App\Swep\Helpers\Helper::toNumber($ta)}}</td>
+                    <td>/month</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            @endif
+            <tr>
+                <td class="b-top">Total Earnings</td>
+                <td class="text-right b-top">{{\App\Swep\Helpers\Helper::toNumber($totalMonthly = $monthly + $pera + $ra + $ta)}}</td>
+                <td class="b-top">/month</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td><br></td>
+                <td><br></td>
+                <td><br></td>
+                <td><br></td>
+                <td><br></td>
+            </tr>
+            <tr>
+                <td class="b-top">Annual</td>
+                <td class="b-top"><br></td>
+                <td class="b-top"><br></td>
+                <td class="text-right b-top">{{\App\Swep\Helpers\Helper::toNumber($monthlyAnnual = $totalMonthly * 12)}}</td>
+                <td class="b-top">/annum</td>
+            </tr>
+        </table>
+        <br>
+        <p style="text-indent: 40px; line-height: 20px; text-align: justify">
+            Further, {{$employee->sex == 'MALE' ? 'he' : 'she'}} receives the following additional yearly remuneration, viz:
+        </p>
+        <table style="width: 80%; margin-left: 15%; font-size: 15px" >
+            @php
+                $otherIncentives = $employee->templateIncentives
+                    ->where('amount','!=',0)
+                    ->where('amount','!=',null)
+                    ->where('incentive_code','!=','MONTHLY')
+                    ->where('incentive_code','!=','PERA')
+                    ->sortByDesc('amount');
+
+            @endphp
+            @if(!empty($otherIncentives))
+                @foreach($otherIncentives as $incentive)
+                    @if($incentive->amount !== null)
+                        <tr>
+                            <td style="width: 60%;">{{$incentive->incentive->description}}</td>
+                            <td class="text-right">{{\App\Swep\Helpers\Helper::toNumber($incentive->amount)}}</td>
+                            <td style="width: 60px">/annum</td>
+                            <td style="width: 100px"></td>
+                            <td style="width: 60px"></td>
+                        </tr>
+                    @endif
+                @endforeach
+                <tr>
+                    <td class="b-top">Total Other Earnings</td>
+                    <td class="b-top"></td>
+                    <td class="b-top"></td>
+                    <td class="text-right b-top">{{\App\Swep\Helpers\Helper::toNumber($totalOther = $otherIncentives->sum('amount'))}}</td>
+                    <td class="b-top">/annum</td>
+
+                </tr>
+
+            @endif
+            <tr>
+                <td><br></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td class="text-strong" style="font-size: 12px">TOTAL GROSS EARNINGS</td>
+                <td class="text-right"></td>
+                <td></td>
+                <td style="width: 100px" class="text-right text-strong">{{\App\Swep\Helpers\Helper::toNumber($monthlyAnnual + $totalOther)}}</td>
+                <td class="text-strong" style="width: 60px">/annum</td>
+            </tr>
+        </table>
+        <br><br>
+
+
+        <p style="text-indent: 40px; line-height: 20px; text-align: justify">
+        This certification is issued for whatever legal purpose it may serve.
+        </p>
+        <br><br>
+        <div style="overflow: auto">
+            <div style="width: 50%; float: right">
+                <p class="text-center">
+                    <b>{{request('signatory_name')}}</b>
+                    <br>
+                    {{request('signatory_position')}}
+                </p>
+            </div>
+        </div>
+
+
+        <br><br>
+
+        <div>
+            <p class="no-margin text-right" style="font-size: 10px;">FM-AFD-HRS-037, Rev. 01</p>
+            <p class="no-margin text-right" style="font-size: 10px;">Effectivity Date: January 8, 2016</p>
+        </div>
     </div>
-</div>
 
+@endsection
 
-<br><br><br><br>
-
-<p class="no-margin text-right" style="font-size: 10px; font-style: italic">FM-AFD-HRS-037, Rev. 01</p>
-<p class="no-margin text-right" style="font-size: 10px; font-style: italic">Effectivity Date: January 08, 2016</p>
-</body>
-</html>
+@section('scripts')
+    <script type="text/javascript">
+        @if(\Illuminate\Support\Facades\Request::has('autoPrint') && \Illuminate\Support\Facades\Request::get('autoPrint') == true)
+        print();
+        @endif
+    </script>
+@endsection
