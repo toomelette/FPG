8b9689dd4b6c93ee8fb65f4199eda6389ccc2135
Gerald <gguance221@gmail.com>
2026-01-20 16:21:57 +0800

    cols for report emplys

--- resources/views/_hru/cos-employees/dtActions.blade.php
+++ resources/views/_hru/cos-employees/dtActions.blade.php
@@ -2,7 +2,7 @@
     <button type="button" data="{{$cosEmployee->hr_cos_employees_slug}}" data-bs-toggle="modal" data-bs-target="#edit-cos-employee-modal" class="btn btn-sm btn-outline-secondary edit-cos-employee-btn">
         <i class="fa fa-edit"></i>
     </button>
-    <a href="{{route('dashboard.employee.index_cos')}}?find={{$cosEmployee->employee->employee_no}}" target="_blank" type="button" data="{{$cosEmployee->hr_cos_employees_slug}}" class="btn btn-sm btn-outline-secondary edit-cos-employee-btn">
+    <a href="{{route('dashboard.employee.index_cos')}}?find={{$cosEmployee?->employee?->employee_no}}" target="_blank" type="button" data="{{$cosEmployee->hr_cos_employees_slug}}" class="btn btn-sm btn-outline-secondary edit-cos-employee-btn">
         <i class="fa fa-user"></i>
     </a>
     <button type="button" data="{{$cosEmployee->hr_cos_employees_slug}}" onclick="delete_data('{{$cosEmployee->hr_cos_employees_slug}}','{{route('dashboard.cos_employees.destroy','slug')}}')" class="btn btn-sm btn-danger delete_jo_employee_btn">
