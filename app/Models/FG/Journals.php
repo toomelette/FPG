7dbbe554056795dd0d1abf918d3a71ab655c81ad
Gerald <gguance221@gmail.com>
2026-07-23 14:14:20 +0800

    proj id to journals

--- app/Models/FG/Journals.php
+++ app/Models/FG/Journals.php
@@ -16,6 +16,23 @@ class Journals extends Model
         //'date' => 'date',
     ];
 
+    public static function boot()
+    {
+        parent::boot();
+        static::updating(function($a){
+            $a->user_updated = \Auth::user()->user_id;
+            $a->ip_updated = request()->ip();
+            $a->updated_at = \Carbon::now();
+        });
+
+        static::creating(function ($a){
+            $a->user_created = \Auth::user()->user_id;
+            $a->ip_created = request()->ip();
+            $a->created_at = \Carbon::now();
+            $a->project_id = \Auth::user()->project_id;
+        });
+    }
+
     /*Relationships*/
     public function entries()
     {
