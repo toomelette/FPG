5616049d193568ddedcbe5b884090bb859c61b95
GJ Lppy <gguance221@gmail.com>
2026-07-30 22:16:06 +0800

    payroll footer

--- app/Http/Controllers/FG/PayrollPreparationController.php
+++ app/Http/Controllers/FG/PayrollPreparationController.php
@@ -352,7 +352,7 @@ class PayrollPreparationController extends Controller
                     $employeeAdjustments
                         ->orderBy('type','desc')
                         ->orderBy('priority')
-                        ->groupBy('code')
+//                        ->groupBy('code')
                     ;
                 },
             ])
